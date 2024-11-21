dc984f80913df1a30de2b7f96c81fd7002d06501
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-11-20 19:56:58 -0600

    Options being passed, Need to handle cors preflight request

--- src/controllers/registerController.tsx
+++ src/controllers/registerController.tsx
@@ -1,25 +1,31 @@
-import { FormData } from '../components/login'
+import { FormData } from "../components/login";
 // import { useNavigate } from 'react-router-dom'
 
-export const Register = async (data: FormData) => { 
-    console.log("Data: ", JSON.stringify(data));
-    const options = {
-            method: "POST",
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            body: JSON.stringify(data)
-           
+export const Register = async (data: FormData) => {
+  console.log("Data: ", JSON.stringify(data));
+  try {
+    // const options = {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(data),
+    // };
+
+    const response = await fetch("http://0.0.0.0:8000",{
+      method: "POST",
+      headers: { 
+          "Content-Type": "application/json", 
+          "Access-Control-Allow-Origin": "*",  
+          "Access-Control-Allow-Methods": "POST",  },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to register user");
     }
-    try{
-        // console.log("data: ", data)
-       fetch("http://0.0.0.0:8000", options)
-        .then(response2 => response2.json())
-        .then(data2 => console.log(data2))
-        .catch(e => console.log(e));
-       
 
-
-
-    }catch{
-        console.log("error")
-    }
-}
+    const info = await response.json();
+    console.log(info);
+  } catch (error) {
+    console.error("Error during registration:", error);
+  }
+};
