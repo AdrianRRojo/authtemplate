159185a6780947030de40070b1299b47970b3d7a
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-11-27 12:20:27 -0600

    Added styling
    Background color, form now centered, color to button

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -7,6 +7,7 @@ export interface FormData {
   email: string;
   password: string;
   phone: string;
+
 }
 
 interface rcMessages {
@@ -104,70 +105,169 @@ export default function Register() {
   };
 
   return (
-    <div className="">
-      <div className="w-full max-w-3xl grid grid-cols-2 gap-4" id="formDiv">
-        <form onSubmit={handleSubmit} className="bg-blue shadow-md rounded px-16 pt-12 pb-16 mb-8 w-">
-          <label htmlFor="fname" className=" ">
-            First Name:{" "}
-          </label>
-          <input
-            id="fname"
-            type="text"
-            name="fname"
-            className="border-2 border-blue-400"
-            onChange={handleInputChange}
-            value={formData.fname}
-            required
-          />
+    <div className="bg-gray-700">
+      <div className="flex flex-row min-h-screen justify-center items-center">
+      <form className="w-7/12 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+          <div className="px-4 py-6 sm:p-8">
+            <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="fname"
+                    type="text"
+                    name="fname"
+                    onChange={handleInputChange}
+                    value={formData.fname}
+                    required
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
+                  />
+                </div>
+              </div>
 
-          <label htmlFor="lname">Last Name:</label>
-          <input
-            id="lname"
-            type="text"
-            name="lname"
-            className="border-2 border-blue-400"
-            onChange={handleInputChange}
-            value={formData.lname}
-            required
-          />
+              <div className="sm:col-span-3">
+                <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
+                  Last name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="lname"
+                    type="text"
+                    name="lname"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
+                    onChange={handleInputChange}
+                    value={formData.lname}
+                    required
+                  />
+                </div>
+              </div>
 
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            className="border-2 border-blue-400"
-            onChange={handleInputChange}
-            value={formData.email}
-            required
-          />
+              <div className="sm:col-span-6">
+                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
+                    onChange={handleInputChange}
+                    value={formData.email}
+                    required
+                  />
+                </div>
+              </div>
 
-          <label htmlFor="password">Password:</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            className="border-2 border-blue-400"
-            onChange={handleInputChange}
-            value={formData.password}
-            required
-          />
+              <div className="sm:col-span-3">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
+                    onChange={handleInputChange}
+                    value={formData.password}
+                    required
+                  />
+                </div>
+              </div>
 
-          <label htmlFor="phoneForm">Phone:</label>
-          <input
-            id="phoneForm"
-            type="phoneForm"
-            name="phoneForm"
-            maxLength={20}
-            className="border-2 border-blue-400"
-            onChange={(e) => formatThePhoneNumber(e.target.value, setPhone)}
-            value={phone}
-          />
+              <div className="sm:col-span-3">
+                <label htmlFor="phoneForm" className="block text-sm/6 font-medium text-gray-900">
+                  Phone Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="phoneForm"
+                    type="text"
+                    name="phoneForm"
+                    maxLength={20}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm/6"
+                    onChange={(e) => formatThePhoneNumber(e.target.value, setPhone)}
+                    value={phone}
+                  />
+                </div>
+              </div>
+              {/* <div className="col-span-full">
+                <label htmlFor="street-address" className="block text-sm/6 font-medium text-gray-900">
+                  Street address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="street-address"
+                    name="street-address"
+                    type="text"
+                    autoComplete="street-address"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
 
-          <button type="submit" onClick={clearState}>Submit</button>
+              <div className="sm:col-span-2 sm:col-start-1">
+                <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">
+                  City
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="city"
+                    name="city"
+                    type="text"
+                    autoComplete="address-level2"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
+                  State
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="region"
+                    name="region"
+                    type="text"
+                    autoComplete="address-level1"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">
+                  ZIP / Postal code
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="postal-code"
+                    name="postal-code"
+                    type="text"
+                    autoComplete="postal-code"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
+                  />
+                </div>
+              </div> */}
+            </div>
+          </div>
+          <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+            <button
+              type="submit"
+              className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+            >
+              Register
+            </button>
+          </div>
         </form>
       </div>
-      {rcMsg.map((msgs) => <li key={msgs.id}>{msgs.message}</li>)}
-    </div>
+      </div>
   );
 }
