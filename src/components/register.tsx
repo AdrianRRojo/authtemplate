0ca04e662b8b3d38c18aa96c0d18cab01cec843e
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-12-01 18:19:17 -0600

    Clean up

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { RegisterController } from "../controllers/registerController";
 import { useNavigate } from "react-router-dom";
-
 export interface FormData {
   fname: string;
   lname: string;
@@ -12,7 +11,6 @@ export interface FormData {
   postal: string;
   password: string;
   phone: string;
-
 }
 
 interface rcMessages {
@@ -36,15 +34,13 @@ export default function Register() {
   });
 
   const [phone, setPhone] = useState("+1 ");
-  useEffect(() => {
+  useEffect(() => {}, [formData.phone]);
 
-  }, [formData.phone]);
-
-  const [rcMsg, setRcMsg] = useState<rcMessages[]>([{id: "", message: ""}]);
+  const [rcMsg, setRcMsg] = useState<rcMessages[]>([{ id: "", message: "" }]);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-
+ 
     try {
       setFormData((prevData) => ({
         ...prevData,
@@ -56,15 +52,11 @@ export default function Register() {
       if (rcResponse) {
         // console.log("rcResponse is not okay:", rcResponse);
         rcResponse.map((msg: string, idx: string) => {
-          setRcMsg((prevData) => [
-            ...prevData,
-            { id: idx, message: msg },
-          ]);
+          setRcMsg((prevData) => [...prevData, { id: idx, message: msg }]);
         });
         // navigate('/home');
         //window.location.reload();
       } else {
-        //console.log("No good:", rcResponse);
         navigate("/");
         window.location.reload();
       }
@@ -74,7 +66,7 @@ export default function Register() {
   };
   const formatThePhoneNumber = (
     value: string,
-    onChange: (phoneNumber: string) => void,
+    onChange: (phoneNumber: string) => void
   ) => {
     // Remove non-digit characters except the '+' symbol
     const cleanedValue = value.replace(/\D/g, "");
@@ -115,23 +107,29 @@ export default function Register() {
 
   const redirect = () => {
     navigate("/login");
-  }
+  };
 
   const [reqBlock, setReqBlock] = useState("hidden");
   const passwordReq = () => {
     setReqBlock("block");
-  }
+  };
   const passwordReqRemove = () => {
     setReqBlock("hidden");
-  }
+  };
   return (
     <div className="bg-sky-700">
       <div className="flex flex-row min-h-screen justify-center items-center">
-      <form onSubmit={handleSubmit} className="w-4/12 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2 ">
+        <form
+          onSubmit={handleSubmit}
+          className="w-4/12 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2 "
+        >
           <div className="px-4 py-6 sm:p-8 flex flex-row justify-center items-center ">
             <div className="grid w-10/12 grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-3">
-                <label htmlFor="fname" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="fname"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   First name
                 </label>
                 <div className="mt-2">
@@ -149,7 +147,10 @@ export default function Register() {
               </div>
 
               <div className="sm:col-span-3">
-                <label htmlFor="lname" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="lname"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   Last name
                 </label>
                 <div className="mt-2">
@@ -167,7 +168,10 @@ export default function Register() {
               </div>
 
               <div className="sm:col-span-6">
-                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="email"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   Email address
                 </label>
                 <div className="mt-2">
@@ -185,7 +189,9 @@ export default function Register() {
               </div>
 
               <div className="sm:col-span-3">
-                <div className={`${reqBlock} absolute inline-flex items-center justify-center max-w-full overflow-hidden rounded-lg bg-slate-50 border-dashed border-2 border-gray-300 shadow`}>
+                <div
+                  className={`${reqBlock} absolute inline-flex items-center justify-center max-w-full overflow-hidden rounded-lg bg-slate-50 border-dashed border-2 border-gray-300 shadow`}
+                >
                   <div className="px-4 py-5 sm:p-6">
                     <ol>
                       <li>Must contain a number</li>
@@ -195,46 +201,66 @@ export default function Register() {
                     </ol>
                   </div>
                 </div>
-                <label htmlFor="password" className="flex items-center text-sm font-medium text-gray-900">
+                <label
+                  htmlFor="password"
+                  className="flex items-center text-sm font-medium text-gray-900"
+                >
                   Password
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.25"
-                       stroke="currentColor" className="w-5 h-5 ml-2" onMouseOverCapture={passwordReq}>
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                          d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"/>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.25"
+                    stroke="currentColor"
+                    className="w-5 h-5 ml-2"
+                    onMouseOverCapture={passwordReq}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+                    />
                   </svg>
-
                 </label>
                 <div className="mt-2">
                   <input
-                      id="password"
-                      type="password"
-                      name="password"
-                      className="block pl-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
-                      onChange={handleInputChange}
-                      value={formData.password}
-                      required
+                    id="password"
+                    type="password"
+                    name="password"
+                    className="block pl-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
+                    onChange={handleInputChange}
+                    value={formData.password}
+                    required
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-3">
-                <label htmlFor="phoneForm" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="phoneForm"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   Phone Number
                 </label>
                 <div className="mt-2">
-                <input
+                  <input
                     id="phoneForm"
                     type="text"
                     name="phoneForm"
                     maxLength={20}
                     className="block pl-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm/6"
-                    onChange={(e) => formatThePhoneNumber(e.target.value, setPhone)}
+                    onChange={(e) =>
+                      formatThePhoneNumber(e.target.value, setPhone)
+                    }
                     value={phone}
                   />
                 </div>
               </div>
               <div className="col-span-full">
-                <label htmlFor="address" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="address"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   Street address
                 </label>
                 <div className="mt-2">
@@ -246,12 +272,16 @@ export default function Register() {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
                     onChange={handleInputChange}
                     value={formData.address}
+                    required
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-2 sm:col-start-1">
-                <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="city"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   City
                 </label>
                 <div className="mt-2">
@@ -263,30 +293,38 @@ export default function Register() {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
                     onChange={handleInputChange}
                     value={formData.city}
+                    required
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-2">
-                <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="region"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   State
                 </label>
                 <div className="mt-2">
-                  <input
+                <input
                     id="region"
                     name="region"
                     type="text"
-                    autoComplete="address-level1"
+                    autoComplete="region"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
                     onChange={handleInputChange}
                     value={formData.region}
+                    required
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-2">
-                <label htmlFor="postal" className="block text-sm/6 font-medium text-gray-900">
-                  ZIP / Postal code
+                <label
+                  htmlFor="postal"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  ZIP code
                 </label>
                 <div className="mt-2">
                   <input
@@ -297,16 +335,19 @@ export default function Register() {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm/6"
                     onChange={handleInputChange}
                     value={formData.postal}
+                    required
                   />
                 </div>
               </div>
             </div>
           </div>
           <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-
             <button
-                onClick={redirect}
-                className="rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Existing User Login</button>
+              onClick={redirect}
+              className="rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+            >
+              Existing User Login
+            </button>
             <button
               type="submit"
               onClick={clearState}
@@ -315,13 +356,16 @@ export default function Register() {
               Register
             </button>
           </div>
-        {rcMsg.map(msgs => (
-            <li className="text-red-500 text-md font-semibold px-5 list-none mb-0.5 text-center"  key={msgs.id}>{msgs.message}</li>
-        ))}
+          {rcMsg.map((msgs) => (
+            <li
+              className="text-red-500 text-md font-semibold px-5 list-none mb-0.5 text-center"
+              key={msgs.id}
+            >
+              {msgs.message}
+            </li>
+          ))}
         </form>
-
-
       </div>
-      </div>
+    </div>
   );
 }
