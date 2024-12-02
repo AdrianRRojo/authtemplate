c30c6b3ec7ac3eb8d2e5ab125539ca257ae28ede
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-12-02 16:46:37 -0600

    Starting NavBar, not much progress made

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,22 @@
+import Cookies from "js-cookie";
+import ar from '../imgs/ar.png';
 
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-export default function Nav(){
-    return(
-        <div className="nav">
-            nav menu
-        </div>
-    )
+export default function Nav() {
+  const getCookie = Cookies.get("Login");
+
+  return (
+    <div className=" bg-slate-400">
+      <div className="flex justify-start"><img src={ar} className="absolute " alt="AR Logo"/></div>
+      <div className="flex justify-end">
+        {getCookie ? (
+          <a href="/account">account</a>
+        ) : (
+          <a href="/login">Login</a>
+        )}
+      </div>
+    </div>
+  );
 }
